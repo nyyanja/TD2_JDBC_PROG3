39ecc278fa8ff328060e1337a9aab921896c0c33
nyyanja <nyanja.ramahavatra@gmail>
2026-01-18 11:46:01 +1100

    Add new class DishIngredient

---
+++ src/main/java/com/Nj/code/DishIngredient.java
@@ -0,0 +1,21 @@
+package com.Nj.code;
+
+public class DishIngredient {
+
+    private Dish dish;
+    private Ingredient ingredient;
+    private Double quantity;
+    private String unit;
+
+    public DishIngredient(Dish dish, Ingredient ingredient, Double quantity, String unit) {
+        this.dish = dish;
+        this.ingredient = ingredient;
+        this.quantity = quantity;
+        this.unit = unit;
+    }
+
+    public Dish getDish() { return dish; }
+    public Ingredient getIngredient() { return ingredient; }
+    public Double getQuantity() { return quantity; }
+    public String getUnit() { return unit; }
+}
