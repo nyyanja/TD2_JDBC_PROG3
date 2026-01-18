0c983044d88dac535b47bbdd7c385effacf4ed52
nyyanja <nyanja.ramahavatra@gmail>
2026-01-18 11:49:44 +1100

    remove depedencies on dish

--- src/main/java/com/Nj/code/Ingredient.java
+++ src/main/java/com/Nj/code/Ingredient.java
@@ -1,74 +1,21 @@
 package com.Nj.code;
 
-import java.util.Objects;
-
 public class Ingredient {
 
     private Integer id;
     private String name;
     private Double price;
     private Category category;
-    private Dish dish;
-    private Double quantity;
 
-    public Ingredient() {}
-
-    // Constructeur utilisé dans Main
-    public Ingredient(Integer id, String name, Double price, Category category, Dish dish) {
+    public Ingredient(Integer id, String name, Double price, Category category) {
         this.id = id;
         this.name = name;
         this.price = price;
         this.category = category;
-        this.dish = dish;
     }
 
     public Integer getId() { return id; }
-    public void setId(Integer id) { this.id = id; }
-
     public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-
     public Double getPrice() { return price; }
-    public void setPrice(Double price) { this.price = price; }
-
     public Category getCategory() { return category; }
-    public void setCategory(Category category) { this.category = category; }
-
-    public Dish getDish() { return dish; }
-    public void setDish(Dish dish) { this.dish = dish; }
-
-    public Double getQuantity() { return quantity; }
-    public void setQuantity(Double quantity) { this.quantity = quantity; }
-
-    public String getDishName() { return dish != null ? dish.getName() : null; }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Ingredient)) return false;
-        Ingredient that = (Ingredient) o;
-        return Objects.equals(id, that.id) &&
-                Objects.equals(name, that.name) &&
-                Objects.equals(price, that.price) &&
-                category == that.category &&
-                Objects.equals(dish, that.dish) &&
-                Objects.equals(quantity, that.quantity);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, price, category, dish, quantity);
-    }
-
-    @Override
-    public String toString() {
-        return "Ingredient{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", price=" + price +
-                ", category=" + category +
-                ", dishName=" + getDishName() +
-                ", quantity=" + quantity +
-                '}';
-    }
 }
