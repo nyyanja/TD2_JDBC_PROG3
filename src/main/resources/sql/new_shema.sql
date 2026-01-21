1f7a3deade6f6bf5de1dd30148d4b0803fbd5e1f
nyyanja <nyanja.ramahavatra@gmail>
2026-01-22 10:13:17 +1100

    TD4: add new sql stock_movement

--- src/main/resources/sql/new_shema.sql
+++ src/main/resources/sql/new_shema.sql
@@ -22,3 +22,16 @@ CREATE TABLE dish_ingredient (
                                  FOREIGN KEY (dish_id) REFERENCES dish(id),
                                  FOREIGN KEY (ingredient_id) REFERENCES ingredient(id)
 );
+
+CREATE TABLE stock_movement (
+                                id SERIAL PRIMARY KEY,
+                                ingredient_id INTEGER NOT NULL,
+                                quantity DOUBLE PRECISION NOT NULL,
+                                unit VARCHAR(10) NOT NULL,
+                                movement_date TIMESTAMP NOT NULL,
+                                FOREIGN KEY (ingredient_id) REFERENCES ingredient(id)
+);
+ALTER TABLE ingredient
+    ADD COLUMN stock_quantity DOUBLE PRECISION NOT NULL,
+ADD COLUMN stock_unit VARCHAR(20) NOT NULL;
+
