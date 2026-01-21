7a377f83c941e364d2192af5e335b6ba7b123321
nyyanja <nyanja.ramahavatra@gmail>
2026-01-22 10:13:59 +1100

    TD4: add new attribut and getter/setters

--- src/main/java/com/Nj/code/Ingredient.java
+++ src/main/java/com/Nj/code/Ingredient.java
@@ -7,15 +7,39 @@ public class Ingredient {
     private Double price;
     private Category category;
 
-    public Ingredient(Integer id, String name, Double price, Category category) {
+    private Double stockQuantity;
+    private String stockUnit;
+
+    public Ingredient(
+            Integer id,
+            String name,
+            Double price,
+            Category category,
+            Double stockQuantity,
+            String stockUnit
+    ) {
         this.id = id;
         this.name = name;
         this.price = price;
         this.category = category;
+        this.stockQuantity = stockQuantity;
+        this.stockUnit = stockUnit;
     }
 
     public Integer getId() { return id; }
     public String getName() { return name; }
     public Double getPrice() { return price; }
     public Category getCategory() { return category; }
+
+    public Double getStockQuantity() { return stockQuantity; }
+    public String getStockUnit() { return stockUnit; }
+
+    public void consumeStock(Double quantity) {
+        if (stockQuantity < quantity) {
+            throw new RuntimeException(
+                    "Stock insuffisant pour " + name
+            );
+        }
+        stockQuantity -= quantity;
+    }
 }
