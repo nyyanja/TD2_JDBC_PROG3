ed87598a579f281c3b5f33ab0be409a03c9ccdb0
nyyanja <nyanja.ramahavatra@gmail>
2026-01-22 16:24:33 +1100

    TD4: modified ingredient

--- src/main/java/com/Nj/code/Ingredient.java
+++ src/main/java/com/Nj/code/Ingredient.java
@@ -1,45 +1,52 @@
 package com.Nj.code;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Ingredient {
 
     private Integer id;
     private String name;
     private Double price;
     private Category category;
-
     private Double stockQuantity;
     private String stockUnit;
+    private List<StockMovement> stockMovementList;
 
-    public Ingredient(
-            Integer id,
-            String name,
-            Double price,
-            Category category,
-            Double stockQuantity,
-            String stockUnit
-    ) {
+    public Ingredient(Integer id, String name, Double price, Category category) {
+        this(id, name, price, category, 0.0, "KG");
+    }
+
+    public Ingredient(Integer id, String name, Double price, Category category, Double stockQuantity, String stockUnit) {
         this.id = id;
         this.name = name;
         this.price = price;
         this.category = category;
         this.stockQuantity = stockQuantity;
         this.stockUnit = stockUnit;
+        this.stockMovementList = new ArrayList<>();
     }
 
     public Integer getId() { return id; }
+    public void setId(Integer id) { this.id = id; }
+
     public String getName() { return name; }
     public Double getPrice() { return price; }
     public Category getCategory() { return category; }
 
     public Double getStockQuantity() { return stockQuantity; }
-    public String getStockUnit() { return stockUnit; }
+    public void setStockQuantity(Double stockQuantity) { this.stockQuantity = stockQuantity; }
 
-    public void consumeStock(Double quantity) {
-        if (stockQuantity < quantity) {
-            throw new RuntimeException(
-                    "Stock insuffisant pour " + name
-            );
+    public String getStockUnit() { return stockUnit; }
+    public void setStockUnit(String stockUnit) { this.stockUnit = stockUnit; }
+
+    public List<StockMovement> getStockMovementList() { return stockMovementList; }
+    public void setStockMovementList(List<StockMovement> stockMovementList) { this.stockMovementList = stockMovementList; }
+
+    public void consumeStock(double quantity) {
+        if (quantity > stockQuantity) {
+            throw new RuntimeException("Stock insuffisant pour " + name);
         }
-        stockQuantity -= quantity;
+        this.stockQuantity -= quantity;
     }
 }
