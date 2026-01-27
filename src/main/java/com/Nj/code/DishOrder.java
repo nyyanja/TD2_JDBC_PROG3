da44feea53b6b68009b7d5ef5db1082aa5fa619b
nyyanja <nyanja.ramahavatra@gmail>
2026-01-28 09:52:00 +1100

    Annexe2:Create new class DishOrder

--- src/main/java/com/Nj/code/DishOrder.java
+++ src/main/java/com/Nj/code/DishOrder.java
@@ -1,4 +1,25 @@
 package com.Nj.code;
 
 public class DishOrder {
+
+    private Integer id;
+    private Dish dish;
+    private Integer quantity;
+
+    public DishOrder() {}
+
+    public DishOrder(Integer id, Dish dish, Integer quantity) {
+        this.id = id;
+        this.dish = dish;
+        this.quantity = quantity;
+    }
+
+    public Integer getId() { return id; }
+    public void setId(Integer id) { this.id = id; }
+
+    public Dish getDish() { return dish; }
+    public void setDish(Dish dish) { this.dish = dish; }
+
+    public Integer getQuantity() { return quantity; }
+    public void setQuantity(Integer quantity) { this.quantity = quantity; }
 }
